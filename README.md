58a2d295897f981fc66afabd192edf7f93199527
Mostafa-Albelbeisi <mostfabelbeisi@gmail.com>
2022-10-12 14:41:45 +0000

    v0.1

---
+++ README.md
@@ -0,0 +1,25 @@
+## LAB - Class 05
+## Project: pythonic-garage-band
+version: 0.1
+
+## Author: **Mostafa Albelbeisi**
+
+**Links and Resources**
+
+**Setup**
+- pip install -r requirements.txt
+- pytest
+
+
+**How to initialize/run your application (where applicable)**
+- git init
+- python -m venv .venv
+- source .venv/bin/activate
+
+**How to use your library (where applicable)** 
+- Pytest
+
+## Tests
+- **How do you run tests?** pytest
+- **Any tests of note?** No
+- **Describe any tests that you did not complete, skipped, etc** 
---
+++ pythonic_garage_band/band.py
@@ -0,0 +1,92 @@
+from abc import abstractmethod, ABC
+from unicodedata import name
+
+# This is first class
+class Band:
+    instances=[] 
+    def __init__(self, name, members):
+        self.name=name
+        self.members=members
+        Band.instances.append(self)
+
+    def play_solos(self):
+        solos=[]
+        for i in self.members:
+             solos.append(i.play_solo())
+        return solos
+    
+    def __str__(self):
+        return f'Our band is {self.name}'
+    def __repr__(self):
+        return f"Band instance. Name = {self.name}"
+
+    @classmethod
+    def to_list(cls) :
+        return cls.instances   
+
+# Here we create a base class, this class will be inherited to other class,
+class Musician:
+        members = []
+        def __init__(self, name):
+            self.name = name
+            # Musician.members.append(self.name)
+        def play_solos(self):
+            return ""
+        def get_instrument(self):
+            return ""
+        def __str__(self):
+            return f"My name is {self.name} and I play {self.get_instrument()}"
+        def __repr__(self):
+            return f"{self.__class__.__name__} instance. Name = {self.name}"
+        def play_solo(self):
+            return self.play_solos()
+
+            
+
+
+class Guitarist(Musician):
+    def __init__(self, name):
+        super().__init__(name)
+        # Here as long as it is inhert from Musician Calss, so we don't to define a new function for to str and repr.
+    # def __str__(self):
+    #     return f"My name is {self.name} and I play guitar"
+    # def __repr__(self):
+    #     return f"Guitarist instance. Name = {self.name}"
+
+    @staticmethod
+    def get_instrument():
+        return "guitar"
+    @staticmethod
+    def play_solo():
+        return "face melting guitar solo"  
+
+
+class Drummer(Musician):
+    def __init__(self, name):
+        super().__init__(name)
+        # And here like Guitarist class
+    # def __str__(self):
+    #     return f"My name is {self.name} and I play drums"
+    # def __repr__(self):
+    #     return f"Drummer instance. Name = {self.name}"
+    @staticmethod
+    def get_instrument():
+        return "drums"
+    @staticmethod
+    def play_solo():
+        return "rattle boom crash"
+    
+class Bassist(Musician):
+    def __init__(self,name):
+        super().__init__(name)
+        #And here the same 
+    # def __str__(self):
+    #     return f"My name is {self.name} and I play bass"
+    # def __repr__(self):
+    #     return f"Bassist instance. Name = {self.name}"
+    @staticmethod
+    def get_instrument():
+        return "bass"
+    @staticmethod
+    def play_solo():
+        return "bom bom buh bom"
